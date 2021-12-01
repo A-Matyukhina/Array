b52bb04d78c8174199d3116ad65382dd9a5f5077
Alexandra Matyukhina <alexandra.paramygina@gmail.com>
2021-12-01 16:09:02 +0300

    modified code

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,5 +1,6 @@
 package com.company;
 
+import java.sql.SQLOutput;
 import java.util.Random;
 
 public class Main {
@@ -11,6 +12,15 @@ public class Main {
         int [] myArray = new int[10];
         for(int i = 0; i < myArray.length; i++){
             myArray[i] = 10 + rand.nextInt(100 - 10 + 1);
+            System.out.print(myArray[i] + " ");
+            if(i == myArray.length - 1){ // I can't do
+                System.out.println("\n");
+            }
+        }
+        for(int i = 0; i< myArray.length; i++){
+            if(i % 2 == 0){
+                myArray[i] *= 2;
+            }
             System.out.print(myArray[i] + " ");
         }
     }
