b91d1e984bf7b2c49978ec2c8c575d7195d9edc6
Alexandra Matyukhina <alexandra.paramygina@gmail.com>
2021-12-01 17:28:30 +0300

    modified code

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -23,5 +23,17 @@ public class Main {
             }
             System.out.print(myArray[i] + " ");
         }
+        int max = myArray[0];
+        int min = myArray[0];
+        for(int i = 0; i < myArray.length -1; i++){
+            if(max <= myArray[i]) {
+                max = myArray[i];
+            }
+            if(min >= myArray[i]){
+                min = myArray[i];
+            }
+        }
+        System.out.println("\n" + "Самый большой элемент массива: " + max);
+        System.out.println("Самый маленький элемент массива: " + min);
     }
 }
