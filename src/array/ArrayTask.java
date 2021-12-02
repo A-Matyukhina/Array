4ccb4d29da3074984af745294c4d70a01b3bba3d
dkudishin <dkudishin@gmail.com>
2021-12-02 17:36:26 +0100

    review

---
+++ src/array/ArrayTask.java
@@ -0,0 +1,63 @@
+package array;
+
+import java.util.Random;
+
+
+public class ArrayTask {
+
+    public static void main(String[] args) {
+
+        /*
+        Создать массив из 10 целых чисел.
+        Заполнить его случайно сгенерированными значениями от 10 до 100.
+        Вывести все элементы массива на экран.
+         */
+
+        Random rand = new Random();
+
+        int[] myArray = new int[10];
+        for (int i = 0; i < myArray.length; i++) {
+            myArray[i] = 10 + rand.nextInt(100 - 10 + 1);
+//            System.out.print(myArray[i] + " ");
+            System.out.print("\t" + myArray[i]);
+//            if(i == myArray.length - 1){ // I can't do
+//                System.out.println("\n");
+//            }
+        }
+        System.out.println();
+
+        /*
+        1.2 (продолжение)
+        После создания и заполнения - элементы с четными индексами увеличить в два раза.
+        Снова показать на экране.
+        */
+        for (int i = 0; i < myArray.length; i++) {
+            if (i % 2 == 0) {
+                myArray[i] *= 2;
+            }
+            System.out.print("\t" + myArray[i]);
+        }
+        System.out.println("\n"); //escape sequence
+
+
+        /*
+        1.3 (продолжение)
+        Найти самый большой и самый маленький элементы массива. Вывести на экран.
+        */
+
+        int max = myArray[0];
+        int min = myArray[0];
+
+        for(int i = 0; i < myArray.length; i++){
+            if(max < myArray[i]) {
+                max = myArray[i];
+            }
+            if(min > myArray[i]){
+                min = myArray[i];
+            }
+        }
+        //TODO: English!
+        System.out.println("\n" + "Самый большой элемент массива: " + max);
+        System.out.println("Самый маленький элемент массива: " + min);
+    }
+}
