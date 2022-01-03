6b061eb2ce597ef6b5269e8c1df07208fdc4061c
Alex Matyukhina <a.matyukhina@icloud.com>
2022-01-03 15:56:03 +0300

    modified Exception

--- src/Exception/InputNumber.java
+++ src/Exception/InputNumber.java
@@ -4,11 +4,29 @@ import java.util.Scanner;
 
 public class InputNumber {
     public static void main(String[] args) {
+        endlessLoopTillSuccess();
+    }
+
+    public static void endlessLoop() {
         Scanner sc = new Scanner(System.in);
-        while(true) {
+        while (true) {
             try {
                 System.out.println("Enter a number");
                 int number = sc.nextInt();
+            } catch (Exception e) {
+                System.out.println("Attention! Enter a number, please! Try again.");
+                sc.next();
+            }
+        }
+    }
+
+    public static void endlessLoopTillSuccess(){
+        Scanner sc = new Scanner(System.in);
+        while (true) {
+            try {
+                System.out.println("Enter a number");
+                int number = sc.nextInt();
+                break;
             } catch (Exception e) {
                 System.out.println("Attention! Enter a number, please! Try again.");
                 sc.next();
