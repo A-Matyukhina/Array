0a5b6be1df7bed44ef0593e0b969822221baadd0
dkudishin <dkudishin@gmail.com>
2022-02-17 18:25:35 +0100

    reviewed during the lesson: literals

---
+++ src/basics/Literals.java
@@ -0,0 +1,18 @@
+package basics;
+
+public class Literals {
+    public void literals() {
+
+        byte bb = /*(byte)*/123;
+        short s = /*(short)*/123;
+
+        int i = 123;
+        long l = 123L;
+        double d = 2.0;
+        float f = 1.5F;
+        char c = 'c';
+        boolean b = true;
+
+        String str = "abc";
+    }
+}
